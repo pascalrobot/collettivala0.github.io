0626cb3f3ca088d32de59440263455900143bba4
pascalrobot <pascaldifilippo@gmail.com>
2024-09-08 01:42:34 +0200

    Committ

--- resources/CCS/script.js
+++ resources/CCS/script.js
@@ -104,3 +104,19 @@ function readMore() {
       moreText.style.display = "inline";
     }
   }  
+
+
+let processScroll = () => {
+let docElem = document.documentElement, 
+  docBody = document.body,
+  scrollTop = docElem['scrollTop'] || docBody['scrollTop'],
+    scrollBottom = (docElem['scrollHeight'] || docBody['scrollHeight']) - window.innerHeight,
+  scrollPercent = scrollTop / scrollBottom * 100 + '%';
+
+// console.log(scrollTop + ' / ' + scrollBottom + ' / ' + scrollPercent);
+
+  document.getElementById("progress-bar").style.setProperty("--scrollAmount", scrollPercent); 
+}
+
+document.addEventListener('scroll', processScroll);
+
