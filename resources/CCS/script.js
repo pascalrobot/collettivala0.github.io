c3fdf724fba62512b6e3d2917d12d6ca78303b2c
pascalrobot <pascaldifilippo@gmail.com>
2024-12-23 02:37:53 +0100

    c

--- resources/CCS/script.js
+++ resources/CCS/script.js
@@ -125,3 +125,22 @@ function myFunction3 () {
   var popup = document.getElementById("myPopup");
   popup.classList.toggle("show");
 }
+
+
+var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+    x[i].style.display = "none";  
+  }
+  x[slideIndex-1].style.display = "block";  
+}
